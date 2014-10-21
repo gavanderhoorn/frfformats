3713401d1702b4d6eed647b3a7240dd6ae171477
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2014-10-21 21:27:52 +0200

    Initial setup.py and module structure.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "frfformats",
+    version = "0.0.1",
+    author = "G.A. vd. Hoorn",
+    author_email = "g.a.vanderhoorn@tudelft.nl",
+    description = ("A Python library for reading (and sometimes writing) Fanuc Robotics file formats."),
+    license = "Apache2.0",
+    packages=['frfformats'],
+    package_dir={'': 'src'},
+)
